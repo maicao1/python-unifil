69f2b4ecddd22bd955c04dc6acf7192264c44f48
maicao1 <maicon.carolino.campos@escola.pr.gov.br>
2025-10-01 17:39:12 -0300

    Add files via upload

---
+++ python/introducao.py
@@ -0,0 +1,15 @@
+idade = 20
+if idade >= 18:
+    print("voce e de maior")
+    print("bloco 1")
+else:
+    print("voce e menor de idade")
+    print("bloco 2")
+
+for i in range(0,11):
+        print(i)
+
+contador = 1
+while contador <=10:
+     print(contador)
+     contador +=1
